fc8ad9655665637ea6465ebb988c8f498bfd2b0d
Fabian Bakkum <fabianbakkum@hotmail.com>
2018-04-29 15:20:13 +0200

    fixed connection issue, now responds with valid JSON and code cleanup

--- WiFiAccessPoint2.0.ino
+++ WiFiAccessPoint2.0.ino
@@ -7,74 +7,78 @@ bool lost = false;
 const int led = D4;
 const int buzzer = D5;
 
-/* Set these to your desired credentials. */
-const char *ssid = "ESPap"; // You can change it according to your ease
-const char *password = "12345678"; // You can change it according to your ease
+// AP configuration
+const int channel = 6;
+const char *ssid = "WiFinder";
+const char *password = "12345678";
 
-ESP8266WebServer server(80); // establishing server at port 80 (HTTP protocol's default port)
+// Webserver listens on port 80
+ESP8266WebServer server(80);
 
-// This function will be called whenever send a GET reuest to 192.168.4.1/status within the local area connection of this ESP module.
+// GET /status
 void statusGet()
 {
   if(lost)
-    server.send(200, "text/json", "{\"lost:\" true}");
+    server.send(200, "text/json", "{\"lost\": true}");
   else
-    server.send(200, "text/json", "{\"lost:\" false}");
+    server.send(200, "text/json", "{\"lost\": false}");
 }
 
 void statusPut() {
   String requestBody = server.arg("plain");
 
-  // Sloppy parsing, but works for now.
-  if(requestBody.indexOf("true") > 0) {
+  // Sloppy JSON parsing, but works for now.
+  if(requestBody.indexOf("true") > 0 && requestBody.indexOf("false") < 0) {
     lost = true;
-  } else {
+  } else if(requestBody.indexOf("false") > 0 && requestBody.indexOf("true") < 0) {
     lost = false;
     digitalWrite(led, LOW);
+  } else {
+    server.send(400);
+    return;
   }
-
-  Serial.println(requestBody);
 
   server.send(204);
 }
 
 void beep() {
-    for(int i=0;i<80;i++)
-    {
-      digitalWrite(buzzer,HIGH);
-      delay(1);//wait for 1ms
-      digitalWrite(buzzer,LOW);
-      delay(1);//wait for 1ms
+    for(int i = 0; i < 80; i++) {
+      digitalWrite(buzzer, HIGH);
+      delay(1);
+      digitalWrite(buzzer, LOW);
+      delay(1);
     }
 
-    for(int i=0;i<100;i++)
-    {
-      digitalWrite(buzzer,HIGH);
-      delay(2);//wait for 2ms
-      digitalWrite(buzzer,LOW);
-      delay(2);//wait for 2ms
+    for(int i = 0; i < 100; i++) {
+      digitalWrite(buzzer, HIGH);
+      delay(2);
+      digitalWrite(buzzer, LOW);
+      delay(2);
     }
 }
 
 void setup() {
   delay(1000);
+
   pinMode(led, OUTPUT);
   pinMode(buzzer, OUTPUT);
+
   Serial.begin(115200);
   Serial.println();
 
-  Serial.print("Configuring access point...");
-  /* You can remove the password parameter if you want the AP to be open. */
-  WiFi.softAP(ssid, password); // --> This line will create a WiFi hotspot.
+  Serial.print("Configuring access point... ");
+  WiFi.disconnect(true);
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(ssid, password, channel);
 
   IPAddress myIP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
   Serial.println(myIP);
-  
+
   server.on("/status", HTTP_GET, statusGet);
   server.on("/status", HTTP_PUT, statusPut);
   server.begin();
-  Serial.println("HTTP server started");
+  Serial.println("HTTP server started!");
 }
 
 void loop() {
@@ -82,6 +86,6 @@ void loop() {
     beep();
     digitalWrite(led, !digitalRead(led));
   }
-  
+
   server.handleClient();
 }
